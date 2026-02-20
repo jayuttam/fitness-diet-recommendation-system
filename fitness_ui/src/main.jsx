f0663604b311e289fa6a161ece3e82b489fd8bae
Jay <arvinduttam991@gmail.com>
2026-02-21 00:39:50 +0530

    Added global index.css for responsiveness

--- fitness_ui/src/main.jsx
+++ fitness_ui/src/main.jsx
@@ -1,6 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'   // 👈 ADD THIS
 
-createRoot(document.getElementById("root")).render(
-  <App />
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
