122439f367b36d813e087dd7d232760e33b4e78d
Jay <arvinduttam991@gmail.com>
2026-02-20 14:00:38 +0530

    Updated ML requirements and config

--- ml_service/models/app.py
+++ ml_service/models/app.py
@@ -87,4 +87,4 @@ def predict():
 # Run Flask App
 # ===============================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=10000)
